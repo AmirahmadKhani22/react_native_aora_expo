cd35b320ceb9c44e73dbb6892a5762066c89cfdb
AmirahmadKhani22 <130404068+AmirahmadKhani22@users.noreply.github.com>
2025-01-16 16:53:39 +0330

    create simple application error page component

--- components/appError.jsx
+++ components/appError.jsx
@@ -3,14 +3,13 @@ import {StatusBar} from "expo-status-bar"
 import {Text} from "react-native"
 
 export default function AppError({message="" , children}) {
-    console.log(message) 
     return (
         <SafeAreaView className="flex-1 items-center justify-center bg-primary">
             <StatusBar style="light" backgroundColor="#161622"/>
             {
                 message ? (
-                    <Text className="text-2xl text-blue">
-                        <Text className="text-bold">Oops!</Text>
+                    <Text className="text-2xl text-[red] bg-primary">
+                        <Text className="font-bold">Oops!{" "}</Text>
                         {message}
                     </Text>
                 ) : children
