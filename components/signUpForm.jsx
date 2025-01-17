c6d98074795cf93e2f97e88714fb31c33f7a182f
AmirahmadKhani22 <130404068+AmirahmadKhani22@users.noreply.github.com>
2025-01-18 01:08:47 +0330

    create sign up form component

---
+++ components/signUpForm.jsx
@@ -0,0 +1,43 @@
+import {useState} from "react"
+import {View} from "react-native"
+import FormField from "@/components/formField"
+import PrimaryButton from "@/components/primaryButton"
+
+export default function SignUpForm() {
+    const [formData , setFormData] = useState({
+        username: "",
+        email: "",
+        password: ""
+    })
+    const [isSubmitting , setIsSubmitting] = useState(false)
+
+    const submit = async event => {
+        // setIsSubmitting(state => !state)
+        // await to send form data through fetch or react query or axios, with method post, in request body
+    }
+
+    return (
+        <View className="mt-14 gap-y-6">
+            <FormField 
+                label="Username"
+                handleTextChange={text => {setFormData(state => ({...state , username: text}))}}
+            />
+            <FormField 
+                label="Email"
+                handleTextChange={text => {setFormData(state => ({...state , email: text}))}}
+                inputMode="email"
+                keyboardType="email-address"
+            />
+            <FormField 
+                label="Password"
+                handleTextChange={text => {setFormData(state => ({...state , password: text}))}}
+            />
+            <PrimaryButton
+                handlePress={submit}
+                isLoading={isSubmitting}
+            >
+                Sign Up
+            </PrimaryButton>
+        </View>
+    )
+}
