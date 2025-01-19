4192d72a4f98d9cd475ef7310d1a00aead0b484e
AmirahmadKhani22 <130404068+AmirahmadKhani22@users.noreply.github.com>
2025-01-20 02:02:28 +0330

    change user collection id

--- appwrite/index.js
+++ appwrite/index.js
@@ -5,7 +5,7 @@ const appWriteConfig = {
     projectId: "678ce9490025b061ba67",
     platform: "com.amirahmadkhani.react_native_aora_expo_tutorial",
     databaseId: "678cf42f002c0c5cdbd1",
-    userCollectionId: "678cf483002ba20202b1",
+    userCollectionId: "678d7d470014f023bfe4",
     videoCollectionId: "678cf4bf00306fac1bf1",
     filesStorageId: "678cf798001853fa7822"
 }
