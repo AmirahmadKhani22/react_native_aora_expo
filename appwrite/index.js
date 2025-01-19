254e49b6dbe9efb5ee47c08122354ffd67eb80e3
AmirahmadKhani22 <130404068+AmirahmadKhani22@users.noreply.github.com>
2025-01-19 16:10:17 +0330

    initilize appwrtie configs

---
+++ appwrite/index.js
@@ -0,0 +1,5 @@
+import {Client, Account, ID} from 'react-native-appwrite'
+
+const client = new Client()
+    .setProject('678ce9490025b061ba67')
+    .setPlatform('com.amirahmadkhani.react_native_aora_expo_tutorial')
