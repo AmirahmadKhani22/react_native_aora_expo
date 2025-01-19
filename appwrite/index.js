ace783a946a54d438edf5dce15b2b35fb7f89b8b
AmirahmadKhani22 <130404068+AmirahmadKhani22@users.noreply.github.com>
2025-01-19 21:55:42 +0330

    initilize appwrtie appwrite sdk

--- appwrite/index.js
+++ appwrite/index.js
@@ -1,5 +1,19 @@
-import {Client, Account, ID} from 'react-native-appwrite'
+import {Client} from 'react-native-appwrite'
+
+const appWriteConfig = {
+    endpoint: "https://cload.appwrite.io/v1",
+    projectId: "678ce9490025b061ba67",
+    platform: "com.amirahmadkhani.react_native_aora_expo_tutorial",
+    databaseId: "678cf42f002c0c5cdbd1",
+    userCollectionId: "678cf483002ba20202b1",
+    videoCollectionId: "678cf4bf00306fac1bf1",
+    filesStorageId: "678cf798001853fa7822"
+}
 
 const client = new Client()
-    .setProject('678ce9490025b061ba67')
-    .setPlatform('com.amirahmadkhani.react_native_aora_expo_tutorial')
+client
+    .setEndpoint(appWriteConfig.endpoint)
+    .setProject(appWriteConfig.projectId)
+    .setPlatform(appWriteConfig.platform)
+
+export {client , appWriteConfig}
