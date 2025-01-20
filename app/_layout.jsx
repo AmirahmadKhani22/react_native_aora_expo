e27f6cf7bf230dd253ffa9a2338835271d7b2253
AmirahmadKhani22 <130404068+AmirahmadKhani22@users.noreply.github.com>
2025-01-20 21:28:05 +0330

    wrap project to redux context provider and pass it created store

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,5 +1,7 @@
 import "@/css/global.css"
 import {useEffect} from "react"
+import {Provider as RTKProvider} from "react-redux"
+import ReduxStore from "../redux/store"
 import {SplashScreen, Stack} from "expo-router"
 import useImportFonts from "@/hooks/importFonts.js"
 import AppError from "@/components/appError"
@@ -22,9 +24,11 @@ export default function RootLayout() {
         )
     }
     return (
-        <Stack>
-            <Stack.Screen name="index" options={{headerShown: false}}/>
-            <Stack.Screen name="(auth)" options={{headerShown: false}}/>
-        </Stack>
+        <RTKProvider store={ReduxStore}>
+            <Stack>
+                <Stack.Screen name="index" options={{headerShown: false}}/>
+                <Stack.Screen name="(auth)" options={{headerShown: false}}/>
+            </Stack>
+        </RTKProvider>
     )
 }
