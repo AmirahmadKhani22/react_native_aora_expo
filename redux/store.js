cfb6dfecd0c24774cd10f9b6a6a16233dbf78c50
AmirahmadKhani22 <130404068+AmirahmadKhani22@users.noreply.github.com>
2025-01-21 01:07:19 +0330

    add data loadded state reducer

--- redux/store.js
+++ redux/store.js
@@ -1,9 +1,11 @@
 import {configureStore} from "@reduxjs/toolkit"
 import userReducer from "./slices/user"
+import isDataLoaddedReducer from "./slices/dataLoadded"
 
 const store = configureStore({
     reducer: {
         user: userReducer,
+        isDataLoadded: isDataLoaddedReducer
     },
 })
 
