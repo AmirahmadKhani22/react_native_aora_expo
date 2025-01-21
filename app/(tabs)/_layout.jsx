ed7aefb8b3b5a1075bc293d0168abf7377391ce5
AmirahmadKhani22 <130404068+AmirahmadKhani22@users.noreply.github.com>
2025-01-21 16:09:37 +0330

    add status bar for tabs group pages

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,14 +1,18 @@
+import {StatusBar} from "expo-status-bar"
 import {Stack} from "expo-router"
 
 export default function TabsLayout() {
     return (
-        <Stack 
-            screenOptions={{headerShown: false}}
-        >
-            <Stack.Screen name="home"/>
-            <Stack.Screen name="bookmarks"/>
-            <Stack.Screen name="create"/>
-            <Stack.Screen name="profile"/>
-        </Stack>
+        <>
+            <StatusBar style="light" backgroundColor="#161622"/>
+            <Stack 
+                screenOptions={{headerShown: false}}
+            >
+                <Stack.Screen name="home"/>
+                <Stack.Screen name="bookmarks"/>
+                <Stack.Screen name="create"/>
+                <Stack.Screen name="profile"/>
+            </Stack>
+        </>
     )
 }
