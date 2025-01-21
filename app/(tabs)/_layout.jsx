09a7265ceeb9bdbf928509bebbb1eb1500baa02c
AmirahmadKhani22 <130404068+AmirahmadKhani22@users.noreply.github.com>
2025-01-21 16:26:23 +0330

    fix bug: change Stack to tabs in tabs group page layout for display tab bar

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,18 +1,18 @@
 import {StatusBar} from "expo-status-bar"
-import {Stack} from "expo-router"
+import {Tabs} from "expo-router"
 
 export default function TabsLayout() {
     return (
         <>
             <StatusBar style="light" backgroundColor="#161622"/>
-            <Stack 
+            <Tabs 
                 screenOptions={{headerShown: false}}
             >
-                <Stack.Screen name="home"/>
-                <Stack.Screen name="bookmarks"/>
-                <Stack.Screen name="create"/>
-                <Stack.Screen name="profile"/>
-            </Stack>
+                <Tabs.Screen name="home"/>
+                <Tabs.Screen name="bookmarks"/>
+                <Tabs.Screen name="create"/>
+                <Tabs.Screen name="profile"/>
+            </Tabs>
         </>
     )
 }
