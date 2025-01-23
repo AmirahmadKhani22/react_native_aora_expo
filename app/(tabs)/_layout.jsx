c0cb2139e6d2437243ec868eb68c8d3951730ed5
AmirahmadKhani22 <130404068+AmirahmadKhani22@users.noreply.github.com>
2025-01-23 22:10:37 +0330

    customize tab bar style and each tab bar item components

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,17 +1,128 @@
 import {StatusBar} from "expo-status-bar"
 import {Tabs} from "expo-router"
+import {TabBarIcon , TabBarLabel} from "@/components/tabBarItem"
 
 export default function TabsLayout() {
     return (
         <>
             <StatusBar style="light" backgroundColor="#161622"/>
             <Tabs 
-                screenOptions={{headerShown: false}}
+                screenOptions={{
+                    headerShown: false,
+                    tabBarStyle: {
+                        minHeight: 85,
+                        backgroundColor: "#161622",
+                        borderTopWidth: 1,
+                        borderTopColor: "#232533"
+                    },
+                    tabBarItemStyle: {
+                        flexDirection: "row",
+                        alignItems: "flex-end",
+                    },
+                    tabBarActiveTintColor: "#ffa001",
+                    tabBarInactiveTintColor: "#cdcde0",
+                }}
             >
-                <Tabs.Screen name="home"/>
-                <Tabs.Screen name="bookmarks"/>
-                <Tabs.Screen name="create"/>
-                <Tabs.Screen name="profile"/>
+                <Tabs.Screen 
+                    name="Home"
+                    options={{
+                        TabBarIcon: ({focused , color , size}) => {
+                            return (
+                                <TabBarIcon 
+                                    name=""
+                                    focused={focused}
+                                    color={color}
+                                    size={size}
+                                />
+                            )
+                        },
+                        TabBarLabel: ({focused , color , position , children}) => {
+                            return (
+                                <TabBarLabel
+                                    focused={focused}
+                                    color={color}
+                                >
+                                    {children}
+                                </TabBarLabel>
+                            )
+                        }
+                    }}
+                />
+                <Tabs.Screen 
+                    name="Bookmark"
+                    options={{
+                        TabBarIcon: ({focused , color , size}) => {
+                            return (
+                                <TabBarIcon 
+                                    name=""
+                                    focused={focused}
+                                    color={color}
+                                    size={size}
+                                />
+                            )
+                        },
+                        TabBarLabel: ({focused , color , position , children}) => {
+                            return (
+                                <TabBarLabel
+                                    focused={focused}
+                                    color={color}
+                                >
+                                    {children}
+                                </TabBarLabel>
+                            )
+                        }
+                    }}
+                />
+                <Tabs.Screen 
+                    name="Create"
+                    options={{
+                        TabBarIcon: ({focused , color , size}) => {
+                            return (
+                                <TabBarIcon 
+                                    name=""
+                                    focused={focused}
+                                    color={color}
+                                    size={size}
+                                />
+                            )
+                        },
+                        TabBarLabel: ({focused , color , position , children}) => {
+                            return (
+                                <TabBarLabel
+                                    focused={focused}
+                                    color={color}
+                                >
+                                    {children}
+                                </TabBarLabel>
+                            )
+                        }
+                    }}
+                />
+                <Tabs.Screen 
+                    name="Profile"
+                    options={{
+                        TabBarIcon: ({focused , color , size}) => {
+                            return (
+                                <TabBarIcon 
+                                    name=""
+                                    focused={focused}
+                                    color={color}
+                                    size={size}
+                                />
+                            )
+                        },
+                        TabBarLabel: ({focused , color , position , children}) => {
+                            return (
+                                <TabBarLabel
+                                    focused={focused}
+                                    color={color}
+                                >
+                                    {children}
+                                </TabBarLabel>
+                            )
+                        }
+                    }}
+                />
             </Tabs>
         </>
     )
