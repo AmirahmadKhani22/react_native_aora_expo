1e4ca20fe763e0f467c8bc03c04d5d29259b9b2c
AmirahmadKhani22 <130404068+AmirahmadKhani22@users.noreply.github.com>
2025-01-25 00:28:32 +0330

    use the created Image component and fix bug: pass empty array as dependency to useMemo for rendering on first mounting

--- components/formField.jsx
+++ components/formField.jsx
@@ -1,6 +1,6 @@
 import {useState , useMemo} from "react"
-import {View , Text , TextInput , TouchableOpacity , Image} from "react-native"
-import {icons} from "@/constants"
+import {View , Text , TextInput , TouchableOpacity} from "react-native"
+import Image from "@/components/image"
 
 const textInputHPadding = 4 * 4
 const textInputHeight = 4 * 16
@@ -21,7 +21,7 @@ export default function FormField({
     const [shownSecureText , setShownSecureText] = useState(false)
     const _isSecureTextRequired = useMemo(() => {
         return isSecureTextRequired ?? label.match(/password/i)
-    })
+    } , [])
     return (
         <View className={`gap-y-2 ${extraContainerStyle ?? ""}`}>
             <Text className={`text-base text-gray-100 font-pmedium ${extraLabelStyle ?? ""}`}>{label}</Text>
@@ -50,9 +50,10 @@ export default function FormField({
                         onPress={event => {setShownSecureText(state => !state)}}
                     >
                         <Image 
-                            source={shownSecureText ? icons["eyeHide"] : icons["eye"]} 
+                            category="icon"
+                            name={shownSecureText ? "eyeHide" : "eye"} 
+                            style={{width: "100%" , height: "100%"}}
                             resizeMode="contain"
-                            className="flex-1"
                         />
                     </TouchableOpacity>
                 }
