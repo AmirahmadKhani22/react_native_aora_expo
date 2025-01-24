7a5bba0015f2e5f21859272aa761ee35d89e068d
AmirahmadKhani22 <130404068+AmirahmadKhani22@users.noreply.github.com>
2025-01-25 02:43:22 +0330

    add ListHeader and EmptyList components to tabs home page FlatList

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,7 +1,27 @@
-import {Text} from "react-native"
+import {SafeAreaView} from "react-native-safe-area-context"
+import {View , FlatList} from "react-native"
+import ListHeader from "@/components/tabs/home/listHeader"
+import EmptyList from "@/components/tabs/emptyList"
+
+const HPadding = 4 * 4
 
 export default function Home() {
     return (
-        <Text>Home</Text>
+        <SafeAreaView className="min-h-[100vh] bg-primary">
+            <View className="">
+                <FlatList
+                    ListHeaderComponent={() => (
+                        <ListHeader />
+                    )}
+                    ListEmptyComponent={() => (
+                        <EmptyList 
+                            HPadding={HPadding}
+                            title="No Videos Found"
+                            subtitle="Be the first one to upload a video"
+                        />
+                    )}
+                />
+            </View>
+        </SafeAreaView>
     )
 }
