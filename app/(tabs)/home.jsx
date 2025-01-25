8198af74f92c00053e4d3c6c85050840e6fb53ea
AmirahmadKhani22 <130404068+AmirahmadKhani22@users.noreply.github.com>
2025-01-25 03:39:02 +0330

    home page component conditional rendering basis on vidoes loading state

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,11 +1,23 @@
 import {SafeAreaView} from "react-native-safe-area-context"
 import {View , FlatList} from "react-native"
+import useVideo from "@/hooks/useVideo"
+import PrimaryIndicator from "@/components/primaryIndicator"
 import ListHeader from "@/components/tabs/home/listHeader"
 import EmptyList from "@/components/tabs/emptyList"
 
 const HPadding = 4 * 4
 
 export default function Home() {
+    const videos = useVideo()
+    if(!videos) {
+        return (
+            <SafeAreaView className="min-h-[100vh] bg-primary">
+                <PrimaryIndicator>
+                    Getting Videos ...
+                </PrimaryIndicator>
+            </SafeAreaView>
+        )
+    }
     return (
         <SafeAreaView className="min-h-[100vh] bg-primary">
             <View className="">
